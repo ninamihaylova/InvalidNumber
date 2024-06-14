d33e96506168ca8cbaef4d4d85d39b2849214bb1
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-14 11:06:47 +0300

    Initial commit

---
+++ InvalidNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿class InvalidNumber
+{
+    public static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        Boolean isValid = number >= 100 && number <= 200 || number == 0;
+        if (!isValid) {
+            Console.WriteLine("invalid");
+        }
+    }
+}
